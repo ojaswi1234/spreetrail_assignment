d86c55489170de03864832ae2ae8c92e3be11025
Ojaswi Bhardwaj <83720694+ojaswi1234@users.noreply.github.com>
2026-06-13 10:06:48 +0000

    done

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,11 @@ import importRoutes from './routes/importRoutes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*'
+    }
+));
 app.use(express.json());
 
 // Routes
